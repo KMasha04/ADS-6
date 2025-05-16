74331045d195e224307b1445c3427fcfceabdf7a
KMasha04 <k.masha04030420@gmail.com>
2025-05-17 00:03:03 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,7 +4,63 @@
 
 template<typename T>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
+private:
+struct Node {
+T data;
+Node* next;
+explicit Node(const T& value) : data(value), next(nullptr) {
+}
+};
+Node* head;
+Node* tail;
+
+public:
+TPQueue() : head(nullptr), tail(nullptr) {
+}
+~TPQueue() {
+   while (head != nullptr) {
+     Node* temp = head;
+     head = head->next;
+     delete temp;
+   }
+}
+ void push(const T& item) {
+   Node* newNode = new Node(item);
+    if (head == nullptr || item.prior > head->data.prior) {
+      newNode->next = head;
+      head = newNode;
+      if (tail == nullptr) {
+         tail = newNode;
+      }
+      return;
+    }
+   Node* current = head;
+   while (current->next != nullptr &&
+     current->next->data.prior >= item.prior) {
+     current = current->next;
+   }
+   newNode->next = current->next;
+   current->next = newNode;
+   if (newNode->next == nullptr) {
+     tail = newNode;
+   }
+ }
+T pop() {
+  if (head == nullptr) {
+     throw std::underflow_error("Queue is empty");
+  }
+  Node* temp = head;
+  T result = head->data;
+  head = head->next;
+   if (head == nullptr) {
+     tail = nullptr;
+   }
+   delete temp;
+   return result;
+}
+bool isEmpty() const {
+   return head == nullptr;
+}
 };
 
 struct SYM {
